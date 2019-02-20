89ff208ecfccb536ec5686e9929575982d8548a5
33honest <411607562@qq.com>
2019-02-20 15:44:57 +0800

    websocket 测试

---
+++ src/main/java/cn/lnj/netty/fifthexample/WebSocketServer.java
@@ -0,0 +1,33 @@
+package cn.lnj.netty.fifthexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+public class WebSocketServer {
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workGroup).channel(NioServerSocketChannel.class).handler(new LoggingHandler(LogLevel.INFO)).childHandler(new WebSocketInitializer());
+
+        try {
+            ChannelFuture channelFuture = bootstrap.bind(new InetSocketAddress(8899)).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+}
